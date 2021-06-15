86fbbc62c88be0f6d2e7a5c9c6c4ebdc75351bb4
Daniel Rueda <danruedalop@gmail.com>
2021-06-15 20:25:53 +0200

    implementado spawn bolitas

---
+++ NoNameActionGame/Assets/Scripts/Objects/AutoDestroy.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoDestroy : MonoBehaviour
+{
+    private float m_destroyTimer = 8.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_destroyTimer -= Time.deltaTime;
+        if(m_destroyTimer <= 0.0f)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
